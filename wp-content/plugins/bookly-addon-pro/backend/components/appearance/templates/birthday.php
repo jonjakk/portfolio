6c68f9928eefb3df7af549640ed1540e3514f3fc
popiaugust <45264013+popiaugust@users.noreply.github.com>
2020-09-08 14:10:07 +0200

    fh
    dfh

--- wp-content/plugins/bookly-addon-pro/backend/components/appearance/templates/birthday.php
+++
@@ -1,4 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<div class="bookly-box bookly-table" id="bookly-js-birthday">
-    <?php echo implode( '', $fields ) ?>
-</div>
--- wp-content/plugins/bookly-addon-pro/backend/components/dialogs/customer/templates/birthday.php
+++
@@ -1,6 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
-<div class="form-group">
-    <label for="birthday"><?php esc_html_e( 'Date of birth', 'bookly' ) ?></label>
-    <input class="form-control" type="text" ng-model=form.birthday id="birthday"
-           ui-date="datePickerOptions" ui-date-format="yy-mm-dd" autocomplete="off" />
-</div>
